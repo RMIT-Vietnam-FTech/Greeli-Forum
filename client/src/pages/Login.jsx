97e3c4eaa03a6a79afa552d295c3cdc9b15efea7
StarkDaiDe <141554508+StarkDaiDe@users.noreply.github.com>
2024-04-29 23:07:42 +0700

    test

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react"
+
+const Login = () => {
+    return (
+        <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+    <div class="col">
+      Column
+    </div>
+  </div>
+</div>
+    )
+}
+
+export default Login
