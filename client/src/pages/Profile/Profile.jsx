98dac7cdb34a89f9c52fe69a7fb3aa63c134213e
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-05-10 11:35:13 +0700

    Add code for editing profile

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -1,12 +1,21 @@
 import "../../scss/custom.scss";
+import "./styles.css";
+import React, { useState } from "react";
 import BasicInfo from "./components/BasicInfo";
 import PostsGallery from "./components/PostsGallery";
 import ProfileShow from "./components/ProfileShow";
 import demoUserInfo from "./data";
-import "./styles.css";
+
 
 const Profile = () => {
 	const user = demoUserInfo[0];
+	const initialBasicInfo = {
+		email: user.email,
+		tel: user.tel,
+		address: user.address,
+		gender: user.gender
+	}
+	const [basicInfo, setBasicInfo] = useState(initialBasicInfo);
 
 	return (
 		<div className="container-fluid profile-container bg-primary-green-900">
@@ -27,10 +36,10 @@ const Profile = () => {
 						<h2 className="fs-4 text-white border-bottom border-white fw-light">
 							Basic Setting
 						</h2>
-						<BasicInfo id={0} type="email" info={user.email} />
-						<BasicInfo id={1} type="tel" info={user.tel} />
-						<BasicInfo id={2} type="address" info={user.address} />
-						<BasicInfo id={3} type="gender" info={user.gender} />
+						<BasicInfo id={0} type="email" info={basicInfo.email} setBasicInfo={setBasicInfo} />
+						<BasicInfo id={1} type="tel" info={basicInfo.tel} setBasicInfo={setBasicInfo} />
+						<BasicInfo id={2} type="address" info={basicInfo.address} setBasicInfo={setBasicInfo} />
+						<BasicInfo id={3} type="gender" info={basicInfo.gender} setBasicInfo={setBasicInfo} />
 					</div>
 
 					{/* Account Setting Section */}
--- client/src/pages/Profile/components/BasicInfo.jsx
+++ client/src/pages/Profile/components/BasicInfo.jsx
@@ -4,23 +4,79 @@ import {
 	MdOutlineTransgender,
 	MdPhone,
 } from "react-icons/md";
+import { useState } from "react";
 
 const BasicInfo = (props) => {
+	const [isEditing, setIsEditing] = useState(false);
+
+	const editInfoHandler = (event) => {
+		const newInput = event.target.value;
+		props.setBasicInfo(preVal => {
+			if (props.type === "email") {
+				return {
+					...preVal,
+					email: newInput
+				}
+			} else if (props.type === "tel") {
+				return {
+					...preVal,
+					tel: newInput
+				}
+			}
+			else if (props.type === "address") {
+				return {
+					...preVal,
+					address: newInput
+				}
+			}
+			else if (props.type === "gender") {
+				return {
+					...preVal,
+					gender: newInput
+				}
+			}
+		}
+		);
+
+	}
+
 	const iconArray = [
 		<MdEmail />,
 		<MdPhone />,
 		<MdLocationOn />,
 		<MdOutlineTransgender />,
 	];
-	return (
-		<div className="container w-100 text-white info-item">
-			<div className="row">
-				<div className="col-1">{iconArray[props.id]}</div>
-				<p className="col-9">{props.info}</p>
-				<p className="col-2">Edit</p>
+	if (isEditing) {
+		return (
+			<div className="container w-100 text-white info-item">
+				<div className="row">
+					<div className="col-1">{iconArray[props.id]}</div>
+					<input
+						type="text"
+						className="col-9"
+						value={props.info}
+						onChange={editInfoHandler}
+					/>
+					<button
+						className="col-2"
+						onClick={() => { setIsEditing(false); }}
+					>
+						Save
+					</button>
+				</div>
 			</div>
-		</div>
-	);
+		);
+	} else {
+		return (
+			<div className="container w-100 text-white info-item">
+				<div className="row">
+					<div className="col-1">{iconArray[props.id]}</div>
+					<p className="col-9">{props.info}</p>
+					<p className="col-2" onClick={() => { setIsEditing(true) }}>Edit</p>
+				</div>
+			</div>
+		);
+	}
 };
 
 export default BasicInfo;
