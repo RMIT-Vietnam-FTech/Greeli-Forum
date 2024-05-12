c7b22a0b2881b214a49af0f010e56c686912c86b
Bao Ho <gbaoo.1102@gmail.com>
2024-05-12 17:26:51 +0700

    update

--- server/routes/topic.js
+++ server/routes/topic.js
@@ -1,8 +1,8 @@
-import Topic from "../models/Topic";
+import Topic from "../models/Topic.js";
 import express from 'express';
 import { createTopic, getTopic, getTopicById } from "../controllers/topic.js";
  
-const routes = express.routes();
+const routes = express.Router();
 
 routes.get('/get', getTopic);
 routes.get("/get/topicId", getTopicById);
