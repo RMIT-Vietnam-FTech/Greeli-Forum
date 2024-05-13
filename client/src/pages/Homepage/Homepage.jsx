3c0eb127f0eea769905e43f82444925b72a3821c
Bao Ho <gbaoo.1102@gmail.com>
2024-05-13 17:47:38 +0700

    done homepage

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./component/header.jsx";
-import Main from "./component/main.jsx";
-import Jumbotron from "./component/jumbotron.jsx";
+import Header from "./component/Header.jsx";
+import Main from "./component/Main.jsx";
+import Jumbotron from "./component/Jumbotron.jsx";
 
 function Homepage() {
   return (
--- client/src/pages/Homepage/component/jumbotron.jsx
+++ client/src/pages/Homepage/component/jumbotron.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import "../style/jumbotron.css";
+import "../style/Jumbotron.css";
 
 export default function Jumbotron() {
 	return (
     <>
-      <div className="jumbotron container-fluid w-100 py-5 text-center">
+      <div className="jumbotron container-fluid py-5 text-center">
         <h1 className="text-light text-center pb-lg-5 pb-sm-3">
           Wanna be a part of our community builder team?
         </h1>
