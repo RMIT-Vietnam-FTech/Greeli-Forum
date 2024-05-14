8061c85c836bcdc48280d709611ec565fb3d787c
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-05-14 18:00:13 +0700

    Debug dev branch

--- client/src/pages/Homepage/Homepage.jsx
+++ client/src/pages/Homepage/Homepage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./component/Header.jsx";
-import Main from "./component/Main.jsx";
-import Jumbotron from "./component/Jumbotron.jsx";
+import Header from "./component/Header";
+import Main from "./component/Main";
+import Jumbotron from "./component/Jumbotron";
 
 function Homepage() {
   return (
