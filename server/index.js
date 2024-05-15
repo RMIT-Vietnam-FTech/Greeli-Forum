4cfba032450cd75930bd804dd5eec4644cbd11aa
StarkDaiDe <141554508+StarkDaiDe@users.noreply.github.com>
2024-05-15 14:07:32 +0700

    update

--- server/index.js
+++ server/index.js
@@ -29,7 +29,9 @@ app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors({
+	origin: ["https://group-project-cosc3060-2024a-ftech.onrender.com"],
+}));
 
 app.use((req, res, next) => {
 	res.setHeader("Content-Security-Policy", "default-src 'self'; style-src 'self' http://localhost:3001;");
