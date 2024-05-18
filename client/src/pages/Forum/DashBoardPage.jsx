d6efcd8ba458c4baf9a790bf970193eb581f6a7d
StarkDaiDe <141554508+StarkDaiDe@users.noreply.github.com>
2024-05-18 11:09:19 +0700

    fix bugs

--- client/src/pages/Forum/DashBoardPage.jsx
+++ client/src/pages/Forum/DashBoardPage.jsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import AuthLeftSideBar from "../../components/forum/AuthLeftSideBar";
 import LeftSideBar from "../../components/forum/EditTextEditor/LeftSideBar";
 import Cookies from "universal-cookie";
+import React, {useState} from "react";
 export default function DashBoardPage() {
 	const cookies = new Cookies();
 	const token = cookies.get("TOKEN");
