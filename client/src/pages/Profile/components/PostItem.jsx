398ab815ed2672ef09dd746ce3ae8a02f9e09051
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-05-19 18:34:13 +0700

    Done Profile responsive

--- client/src/pages/Profile/components/PostItem.jsx
+++ client/src/pages/Profile/components/PostItem.jsx
@@ -1,6 +1,6 @@
 import { IoMdMore } from "react-icons/io";
 import { MdInsertComment, MdShare } from "react-icons/md";
-import { TbArrowBigDown,  } from "react-icons/tb";
+import { TbArrowBigDown } from "react-icons/tb";
 
 const PostItem = (props) => {
 	const {
@@ -17,56 +17,48 @@ const PostItem = (props) => {
 	return (
 		<div className="bg-primary-green post-item px-3 py-3 mb-3">
 			<div className="w-100 d-flex justify-content-end">
-				<IoMdMore size={"2vw"} color={"white"} />
+				<IoMdMore size={"28px"} color={"white"} />
 			</div>
-			<div className="container">
-				<div className="row w-100">
-					<div className="col-9 main-content">
-						<div className="d-flex flex-row post-author">
-							<img
-								className="rounded-circle"
-								src="https://via.placeholder.com/50"
-								alt="author"
-							/>
-							<div className="d-flex flex-column justify-content-center">
-								<div className="w-100 d-flex flex-row justify-content-around">
-									<p className="text-primary-yellow fw-bold m-0">
-										{thread}/{title}
-									</p>
-									<p className="text-white m-0">
-										{createdDate}
-									</p>
-								</div>
-								<p className="text-white p-0 m-0">{author}</p>
+			<div className="d-flex w-100 container">
+				<div className="post-main-content">
+					<div className="d-flex flex-row post-author">
+						<img
+							className="rounded-circle"
+							src="https://via.placeholder.com/50"
+							alt="author"
+						/>
+						<div className="d-flex flex-column justify-content-center">
+							<div className="w-100 d-flex flex-row flex-sm-nowrap flex-wrap justify-content-start justify-content-sm-around">
+								<p className="text-primary-yellow fw-bold m-0">
+									{thread}/{title}
+								</p>
+								<p className="text-white m-0">{createdDate}</p>
 							</div>
+							<p className="text-white p-0 m-0">{author}</p>
 						</div>
-						<p className="text-white mt-3">{content}</p>
-						<div className="d-flex justify-content-start interaction-menu">
-							<div className="bg-primary-green-900 text-white d-flex flex-row justify-content-around align-items-center py-2 upvote">
-								{/* <TbArrowBigUpFilled
+					</div>
+					<p className="w-100 text-white mt-3">{content}</p>
+					<div className="d-flex justify-content-start interaction-menu">
+						<div className="bg-primary-green-900 text-white d-flex flex-row justify-content-start align-items-center py-2 px-2 upvote">
+							{/* <TbArrowBigUpFilled
 									size={"2vw"}
 									color={"white"}
 								/> */}
-								<p className="p-0 m-0">{upvote}</p>
-								<TbArrowBigDown size={"2vw"} color={"white"} />
-							</div>
-							<div className="bg-primary-green-900 text-white d-flex flex-row justify-content-around align-items-center py-2 comment">
-								<MdInsertComment size={"2vw"} color={"white"} />
-								<p className="p-0 m-0">{comment}</p>
-							</div>
-							<div className="bg-primary-green-900 text-white d-flex flex-row justify-content-around align-items-center py-2 share">
-								<MdShare size={"2vw"} color={"white"} />
-								<p className="p-0 m-0">Share</p>
-							</div>
+							<p className="p-0 m-0">{upvote}</p>
+							<TbArrowBigDown size={"2vw"} color={"white"} />
+						</div>
+						<div className="bg-primary-green-900 text-white d-flex flex-row justify-content-start align-items-center py-2 px-2 comment">
+							<MdInsertComment size={"2vw"} color={"white"} />
+							<p className="p-0 m-0">{comment}</p>
+						</div>
+						<div className="bg-primary-green-900 text-white d-flex flex-row justify-content-start align-items-center py-2 px-2 share">
+							<MdShare size={"2vw"} color={"white"} />
+							<p className="p-0 m-0">Share</p>
 						</div>
 					</div>
-					<div className="position-relative col-3">
-						<img
-							className="post-img position-absolute my-auto"
-							src={postImgURL}
-							alt="post"
-						/>
-					</div>
+				</div>
+				<div className="d-none d-sm-block">
+					<img className="post-img" src={postImgURL} alt="post" />
 				</div>
 			</div>
 		</div>
