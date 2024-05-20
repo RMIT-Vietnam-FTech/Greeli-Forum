c840fadb47de49b06ff34bb0b446d90aede43ad7
StarkDaiDe <141554508+StarkDaiDe@users.noreply.github.com>
2024-05-21 00:38:15 +0700

    update upload image

--- client/src/pages/Profile/components/ProfileShow.jsx
+++ client/src/pages/Profile/components/ProfileShow.jsx
@@ -19,7 +19,7 @@ const ProfileShow = (props) => {
 		formData.append("image", preview);
 		const configuration = {
 			method: "post",
-			url: `http://localhost:3001/api/upload/${userId}`,
+			url: `/api/upload/${userId}`,
 			headers: {
 				"Content-Type": "multipart/form-data",
 			},
