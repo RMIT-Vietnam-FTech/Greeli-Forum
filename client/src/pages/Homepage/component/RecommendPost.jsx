04abad8b1b35046f52d6f853db72b8aba993cd1d
Bao Ho <gbaoo.1102@gmail.com>
2024-05-23 03:26:36 +0700

    fix homepage 22/05

--- client/src/pages/Homepage/component/RecommendPost.jsx
+++ client/src/pages/Homepage/component/RecommendPost.jsx
@@ -1,15 +1,16 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, lazy } from "react";
 import Slider from "react-slick";
+import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
 import axios from "axios";
 
 function Posts() {
 	const Post = (props) => {
 		return (
-			<div className="news-card card-item rounded-3">
+			<div className="news-card card-item rounded-4">
 				<img
 					src={props.img}
 					alt="News"
-					className="card-img-top h-50 object-fit-cover rounded-top-3"
+					className="card-img-top h-50 object-fit-cover rounded-top-4"
 				/>
 				<div className="cards-body h-auto">
 					<p className="card-date">
@@ -20,73 +21,120 @@ function Posts() {
 			</div>
 		);
 	};
+	const NextArrow = (props) => {
+    const { style, onClick } = props;
+    return (
+      <div
+        style={{
+          ...style,
+          display: "block",
+          position: "absolute",
+          top: "50%",
+          right: "-2rem",
+          transform: "translateY(-50%)",
+          zIndex: 2, // Ensure arrows are on top of the slider
+          cursor: "pointer",
+        }}
+        onClick={onClick}
+      >
+        <FaArrowRightLong className="text-greeli-emphasis" size={30} />
+      </div>
+    );
+  };
+
+  const PrevArrow = (props) => {
+    const { style, onClick } = props;
+    return (
+      <div
+        style={{
+          ...style,
+          display: "block",
+          position: "absolute",
+          top: "50%",
+          left: "-2rem",
+          zIndex: 2, // Ensure arrows are on top of the slider
+          cursor: "pointer",
+        }}
+        onClick={onClick}
+      >
+        <FaArrowLeftLong className="text-greeli-emphasis" size={30} />
+      </div>
+    );
+  };
 
 	const settings = {
 		dots: false,
-		infinite: false,
-		speed: 500,
+		accessibility: true,
+		speed: 300,
 		slidesToShow: 4,
-		slidesToScroll: 4,
+		slidesToScroll: 1,
+		swipeToSlide: true,
+		swipe: true,
 		initialSlide: 0,
 		lazyLoad: true,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
 		responsive: [
-			{
-				breakpoint: 993,
-				settings: {
-					slidesToShow: 2, // Change to 2 cards for tablets
-					slidesToScroll: 2,
-					infinite: true,
-				},
+		{
+			breakpoint: 993,
+			settings: {
+			slidesToShow: 2, // Change to 2 cards for tablets
+			swipeToSlide: true,
+			slidesToScroll: 1,
+			infinite: true,
 			},
-			{
-				breakpoint: 576,
-				settings: {
-					slidesToShow: 1, // Change to 1 card for mobile
-					slidesToScroll: 1,
-					infinite: true,
-				},
+		},
+		{
+			breakpoint: 576,
+			settings: {
+			slidesToShow: 1, // Change to 1 card for mobile
+			swipeToSlide: true,
+			slidesToScroll: 1,
+			infinite: true,
+			fade: true,
 			},
+		},
 		],
 	};
 
 	// Fetch data from API
 	const [data, setData] = useState([]);
-	useEffect(() => {
-		// Fetch data using Axios when component mounts
-		axios
-			.get("http://localhost:3001/api/v1/news/get")
-			.then((response) => {
-				setData(response.data);
-				console.log(response.data);
-			})
-			.catch((error) => {
-				console.error("Error fetching data:", error);
-			});
-	}, []);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/api/v1/news/get")
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
-	const sliderRef = useRef();
-	const handleWheel = (e) => {
-		if (e.deltaX > 0) {
-			sliderRef.current.slickNext();
-		}
-		if (e.deltaX < 0) {
-			sliderRef.current.slickPrev();
-		}
-	};
+  const sliderRef = useRef();
+  const handleWheel = (e) => {
+    if (e.deltaX > 0) {
+      sliderRef.current.slickNext();
+    }
+    if (e.deltaX < 0) {
+      sliderRef.current.slickPrev();
+    }
+  };
 
-	return (
-		<div onWheel={handleWheel}>
-			<Slider {...settings} className="slider-container" ref={sliderRef}>
-				{data.map((item) => (
-					<Post
-						key={item.id}
-						img={item.image}
-						date={item.date}
-						title={item.title}
-					/>
-				))}
-			</Slider>
-		</div>
-	);
+  return (
+    <div onWheel={handleWheel}>
+      <Slider {...settings} className="slider-container" ref={sliderRef}>
+        {data.map((item) => (
+          <Post
+            key={item.id}
+            img={item.image}
+            date={item.date}
+            title={item.title}
+          />
+        ))}
+      </Slider>
+    </div>
+  );
 }
+
 export default Posts;
