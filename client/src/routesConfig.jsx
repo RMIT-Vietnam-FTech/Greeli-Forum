7924053947cbdcbfd61a8cadffa6cf3b2325edc2
Qui Huynh Le Diem <huynhlediemqui.vn@gmail.com>
2024-05-27 15:11:59 +0700

    Merge and debug

--- client/src/routesConfig.jsx
+++ client/src/routesConfig.jsx
@@ -19,33 +19,43 @@ import Sitemap from "./pages/Sitemap/Sitemap.jsx";
 import ForumPage from "./pages/Forum/ForumPage/ForumPage.jsx";
 import NewPassword from "./pages/Login/NewPassword.jsx";
 import ResetPassword from "./pages/Login/ResetPassword.jsx";
+import AdminDashboard from "./pages/AdminDashboard/AdminDashboard";
 import Cookies from "./components/Cookies/Cookie.jsx";
 
 const routesConfig = [
-  { path: '/', element: <Homepage />, name: 'Homepage' },
-  { path: '/login', element: <Login />, name: 'Login' },
-  { path: '/register', element: <Register />, name: 'Register' },
-  { path: '/general', element: <GeneralPage />, name: 'General' },
-  { path: '/contact', element: <ContactPage />, name: 'Contact' },
-  // { path: '/upload', element: <Upload />, name: 'Upload' },
-  { path: '/sitemap', element: <Sitemap />, name: 'Sitemap' },
-  { path: '/resetPassword', element: <ResetPassword />, name: 'Reset Password' },
-  { path: '/resetPassword/:token/:userId', element: <NewPassword />, name: 'Create New Password' },
-  {
-    element: <RequireAuth />,
-    children: [
-      {
-        element: <RequireActivate />,
-        children: [
-          { path: '/profile', element: <Profile />, name: 'Profile' },
-          { path: '/user/:userId', element: <Profile />, name: 'User Profile' },
-          { path: '/chat', element: <Chat />, name: 'Chat' },
-        ],
-      },
-    ],
-  },
-  { path: '/forum/*', element: <ForumPage />, name: 'Forum' },
-  { path: '*', element: <ErrorPage />, name: 'Error' },
+	{ path: "/", element: <Homepage />, name: "Homepage" },
+	{ path: "/login", element: <Login />, name: "Login" },
+	{ path: "/register", element: <Register />, name: "Register" },
+	{ path: "/general", element: <GeneralPage />, name: "General" },
+	{ path: "/contact", element: <ContactPage />, name: "Contact" },
+	// { path: '/upload', element: <Upload />, name: 'Upload' },
+	{ path: "/sitemap", element: <Sitemap />, name: "Sitemap" },
+	{
+		path: "/resetPassword",
+		element: <ResetPassword />,
+		name: "Reset Password",
+	},
+	{
+		path: "/resetPassword/:token/:userId",
+		element: <NewPassword />,
+		name: "Create New Password",
+	},
+	{ path: "/admin", element: <AdminDashboard />, name: "Admin Dashboard" },
+	{
+		element: <RequireAuth />,
+		children: [
+			{
+				element: <RequireActivate />,
+				children: [
+					{ path: "/profile", element: <Profile />, name: "Profile" },
+					{ path: "/user/:userId", element: <Profile />, name: "User Profile" },
+					{ path: "/chat", element: <Chat />, name: "Chat" },
+				],
+			},
+		],
+	},
+	{ path: "/forum/*", element: <ForumPage />, name: "Forum" },
+	{ path: "*", element: <ErrorPage />, name: "Error" },
 ];
 
 export default routesConfig;
