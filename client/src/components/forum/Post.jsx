e26ce7715d83cfb0541e94c10abd971e403b9c37
Pham Nhat Minh <s4019811@rmit.edu.vn>
2024-05-29 03:37:39 +0700

    done forum

--- client/src/components/forum/Post.jsx
+++ client/src/components/forum/Post.jsx
@@ -6,28 +6,31 @@ import DropDown from "./DropDown";
 import EditTextEditor from "./EditTextEditor/EditTextEditor";
 import ImageOrVideo from "./ImageOrVideo";
 
+import { Link, useNavigate } from "react-router-dom";
 import { AuthorizationContextProvider } from "../../context/AuthorizationContext";
 import { EditContextProvider } from "../../context/EditContext";
-import { Link, useNavigate } from "react-router-dom";
 
 import { Button } from "react-bootstrap";
-import ButtonUpvote from "./ButtonUpvote";
 import { ButtonComment } from "../../pages/Forum/PostPage/PostComment";
+import ButtonUpvote from "./ButtonUpvote";
 
 import { useLogin } from "../../hooks/useLogin";
 
-import { IoMdClose } from "react-icons/io";
-import { IoMdCheckmark } from "react-icons/io";
 import { BsShieldFillX } from "react-icons/bs";
 import { BsShieldFillCheck } from "react-icons/bs";
+import { IoMdClose } from "react-icons/io";
+import { IoMdCheckmark } from "react-icons/io";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import Skeleton from "react-loading-skeleton";
 
+axios.defaults.withCredentials = true;
+
 dayjs.extend(relativeTime);
 
 export default function Post({ postData, isThreadAdmin }) {
+<<<<<<< HEAD
   const isLogin = useLogin();
   const [newComment, setNewComment] = useState([]);
   const [isApproved, setIsApproved] = useState(postData.isApproved);
@@ -55,26 +58,53 @@ export default function Post({ postData, isThreadAdmin }) {
       }
     );
   }
+=======
+	const isLogin = useLogin();
+	const [newComment, setNewComment] = useState([]);
+	const [isApproved, setIsApproved] = useState(postData.isApproved);
+	const navigate = useNavigate();
 
-  async function handleUnApproved() {
-    try {
-      //delete and redirect
-      const path = `http://localhost:3001/api/v1/posts/${postData._id}`;
-      await axios.delete(
-        path,
+	const handleUserProfileRedirect = () => {
+		navigate(`/user/${postData.createdBy.userId}`, { root: true });
+	};
 
-        {
-          data: {
-            threadId: postData.belongToThread,
-          },
-          headers: {
-            Authorization: `Bearer ${
-              JSON.parse(localStorage.getItem("user")).token
-            }`,
-          },
-        }
-      );
+	async function handleApproved() {
+		setIsApproved(true);
+		const path = `http://localhost:3001/api/v1/admin/posts/${postData._id}`;
+		await axios.put(
+			path,
+			{ threadId: postData.belongToThread },
+			{
+				headers: {
+					Authorization: `Bearer ${
+						JSON.parse(localStorage.getItem("user")).token
+					}`,
+				},
+			},
+		);
+	}
+>>>>>>> origin/development
 
+	async function handleUnApproved() {
+		try {
+			//delete and redirect
+			const path = `http://localhost:3001/api/v1/posts/${postData._id}`;
+			await axios.delete(
+				path,
+
+				{
+					data: {
+						threadId: postData.belongToThread,
+					},
+					headers: {
+						Authorization: `Bearer ${
+							JSON.parse(localStorage.getItem("user")).token
+						}`,
+					},
+				},
+			);
+
+<<<<<<< HEAD
       navigate(`/forum/${postData.belongToThread}`);
     } catch (error) {
       console.error(error.message);
@@ -122,6 +152,40 @@ export default function Post({ postData, isThreadAdmin }) {
           </div>
 
           {/* <AuthorizationContextProvider
+=======
+			navigate(`/forum/${postData.belongToThread}`);
+		} catch (error) {
+			console.error(error.message);
+		}
+	}
+
+	return (
+		<div
+			className="mx-auto position-relative p-3 my-2"
+			style={{ width: "95%" }}
+		>
+			<EditContextProvider>
+				<div className="w-100 d-flex">
+					{/*post header*/}
+					<div
+						className="d-flex gap-2"
+						onClick={handleUserProfileRedirect}
+					>
+						<Avatar
+							size="sm"
+							src={postData.createdBy.profileImage}
+						/>
+						<p className="mb-0 text-general-emphasis">
+							{postData.createdBy.username}
+						</p>
+						<li className="text-greeli-emphasis">
+							{postData.isApproved
+								? dayjs().to(dayjs(postData.verifiedAt))
+								: dayjs().to(dayjs(postData.createdAt))}
+						</li>
+					</div>
+					{/* <AuthorizationContextProvider
+>>>>>>> origin/development
               componentType="post"
               objectId={postData._id}
             >
@@ -131,8 +195,9 @@ export default function Post({ postData, isThreadAdmin }) {
                 threadId={postData.belongToThread}
               />
             </AuthorizationContextProvider> */}
-        </div>
+				</div>
 
+<<<<<<< HEAD
         {/*-------------------------------------------post body -----------------------------------------*/}
         <div className=" mt-3 w-100">
           <Link
@@ -173,63 +238,107 @@ export default function Post({ postData, isThreadAdmin }) {
           </a>
         </div>
       </EditContextProvider>
+=======
+				{/*post body*/}
+				<div className=" mt-3 w-100">
+					<Link
+						to={`/forum/threads/${postData.belongToThread}/posts/${postData._id}`}
+					>
+						<div
+							tabIndex="0"
+							className="fs-5 fw-bold text-greeli-emphasis"
+							style={{ wordBreak: "break-word" }}
+						>
+							{postData.title}
+						</div>
+					</Link>
 
-      <div className="d-flex justify-content-between align-items-center mb-3 z-3">
-        {/* ----------------------------------- Upvote and Comment Button ------------------------------------- */}
-        <div className="d-flex gap-2">
-          <ButtonUpvote upvote={postData.upvote} postId={postData._id} />
-          <Link
-            to={`/forum/threads/${postData.belongToThread}/posts/${postData._id}`}
-          >
-            <ButtonComment commentLength={postData.comments.length} />
-          </Link>
-        </div>
+					{postData.uploadFile ? (
+						<div
+							className=" my-4 bg-forum-subtle rounded-3 d-flex justify-content-center overflow-hidden"
+							style={{ height: "45vh" }}
+						>
+							<ImageOrVideo
+								alt={postData.createdBy.username}
+								src={postData.uploadFile}
+								isPost={false}
+							/>
+						</div>
+					) : null}
+					<Link
+						to={`/forum/threads/${postData.belongToThread}/posts/${postData._id}`}
+						className="cursor-pointer"
+					>
+						<EditTextEditor
+							componentType="post"
+							content={JSON.parse(postData.content)}
+						/>
+					</Link>
+				</div>
+			</EditContextProvider>
+>>>>>>> origin/development
 
-        {/* ----------------------------------- Verify Status ------------------------------------- */}
+			<div className="d-flex justify-content-between align-items-center mb-3 z-3">
+				{/* ----------------------------------- Upvote and Comment Button ------------------------------------- */}
+				<div className="d-flex gap-2">
+					<ButtonUpvote
+						upvote={postData.upvote}
+						postId={postData._id}
+					/>
+					<Link
+						to={`/forum/threads/${postData.belongToThread}/posts/${postData._id}`}
+					>
+						<ButtonComment
+							commentLength={postData.comments.length}
+						/>
+					</Link>
+				</div>
 
-        {/* for thread Admin  */}
-        {isLogin && isThreadAdmin && !isApproved ? (
-          <div className="d-flex gap-2 me-2">
-            <Button
-              onClick={handleApproved}
-              className="border-greeli rounded-circle bg-transparent text-forum-emphasis"
-            >
-              <IoMdCheckmark />
-            </Button>
+				{/* ----------------------------------- Verify Status ------------------------------------- */}
 
-            <Button
-              onClick={handleUnApproved}
-              className="border-greeli rounded-circle bg-transparent text-forum-emphasis"
-            >
-              <IoMdClose />
-            </Button>
-          </div>
-        ) : (
-          // for post creator
-          <>
-            {isLogin &&
-            postData.createdBy.userId ===
-              JSON.parse(localStorage.getItem("user")).id ? (
-              <div className="d-flex align-items-center">
-                <p
-                  className={`text-forum-emphasis p-0 m-0 ${
-                    isApproved ? null : "opacity-25"
-                  }`}
-                >
-                  {isApproved ? "Approved" : "Unapproved"}
-                </p>
-                <div className="ms-3 fs-4">
-                  {isApproved ? (
-                    <BsShieldFillCheck className="text-success" />
-                  ) : (
-                    <BsShieldFillX className="text-danger" />
-                  )}
-                </div>
-              </div>
-            ) : null}
-          </>
-        )}
-      </div>
-    </div>
-  );
+				{/* for thread Admin  */}
+				{isLogin && isThreadAdmin && !isApproved ? (
+					<div className="d-flex gap-2 me-2">
+						<Button
+							onClick={handleApproved}
+							className="border-greeli rounded-circle bg-transparent text-forum-emphasis"
+						>
+							<IoMdCheckmark />
+						</Button>
+
+						<Button
+							onClick={handleUnApproved}
+							className="border-greeli rounded-circle bg-transparent text-forum-emphasis"
+						>
+							<IoMdClose />
+						</Button>
+					</div>
+				) : (
+					// for post creator
+					<>
+						{isLogin &&
+						postData.createdBy.userId ===
+							JSON.parse(localStorage.getItem("user")).id ? (
+							<div className="d-flex align-items-center">
+								<p
+									className={`text-forum-emphasis p-0 m-0 ${
+										isApproved ? null : "opacity-25"
+									}`}
+								>
+									{isApproved ? "Approved" : "Unapproved"}
+								</p>
+								<div className="ms-3 fs-4">
+									{isApproved ? (
+										<BsShieldFillCheck className="text-success" />
+									) : (
+										<BsShieldFillX className="text-danger" />
+									)}
+								</div>
+							</div>
+						) : null}
+					</>
+				)}
+			</div>
+		</div>
+	);
 }
